09ef218b69cd49b621e65bba544ac71a221bd971
bishnulimbu <subashlimbu987654321@gmail.com>
2024-01-09 10:10:59 +0545

    dsfa

---
+++ two.js
@@ -0,0 +1,10 @@
+const nam =[ 
+  'ram', 'sam', 'hari'
+];
+// let pam = nam.forEach(rep => {
+//     console.log(rep + " limbu");
+// }); 
+let pam = nam.map((item) => {
+  console.log(item + " limbu");
+  
+});
